26c57d6806df4b9202f0c343f817600ab417e5e3
yofu <y.of.u.142857@gmail.com>
2021-02-17 10:42:52 +0900

    add cmd/rz2cat

---
+++ cmd/rz2cat/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"path/filepath"
+
+	"github.com/yofu/rz2"
+)
+
+func ReadData(datdir string, fns ...string) error {
+	for _, fn := range fns {
+		records, err := rz2.ReadServerRecord(filepath.Join(datdir, fn))
+		if err != nil {
+			return err
+		}
+		for _, rec := range records {
+			fmt.Println(rec.Topic)
+		}
+	}
+	return nil
+}
+
+func main() {
+	datdir := flag.String("dir", ".", "dat directory")
+	flag.Parse()
+	datfn := flag.Args()
+	err := ReadData(*datdir, datfn...)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
