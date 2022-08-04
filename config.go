ae5e2ad7bf623c5e7eea5d65bac3213b7bf077dc
yofu <y.of.u.142857@gmail.com>
2022-08-04 12:16:02 +0900

    add config.go

---
+++ config.go
@@ -0,0 +1,44 @@
+package rz2
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	toml "github.com/pelletier/go-toml/v2"
+)
+
+type Config struct {
+	Server string `toml:"server"`
+	Cafile string `toml:"cafile"`
+	Crtfile string `toml:"crtfile"`
+	Keyfile string `toml:"keyfile"`
+	Homedir string `toml:"homedir"`
+	Removehour int `toml:"removehour"`
+	List []string `toml:"list"`
+}
+
+func (c *Config) Println() {
+	fmt.Printf("server: %s\n", c.Server)
+	fmt.Printf("cafile: %s\n", c.Cafile)
+	fmt.Printf("crtfile: %s\n", c.Crtfile)
+	fmt.Printf("keyfile: %s\n", c.Keyfile)
+	fmt.Printf("homedir: %s\n", c.Homedir)
+	fmt.Printf("removehour: %d\n", c.Removehour)
+	fmt.Print("list:\n")
+	for i, t := range c.List {
+		fmt.Printf("    %d: %s\n", i, t)
+	}
+	fmt.Println("")
+}
+
+func (c *Config) ReadConfig(fn string) error {
+	b, err := ioutil.ReadFile(fn)
+	if err != nil {
+		return err
+	}
+	fmt.Println(b)
+	fmt.Println(c.List)
+	toml.Unmarshal(b, &c)
+	fmt.Println(c.List)
+	return nil
+}
